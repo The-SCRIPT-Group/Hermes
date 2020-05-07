5f7eff0ae8bd24168337516334cd3d495352a379
Akhil Narang <me@akhilnarang.dev>
2020-05-08 01:23:41 +0530

    Hermes: switch back to waitress
      * Fixes atleast one weird error -- still need to see about timeout
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@
 [[ -d "venv" ]] || python3.8 -m venv ./venv
 source venv/bin/activate
 pip install -r requirements.txt
-gunicorn web_app:app --workers="$(nproc)"
+waitress-serve --port=8000 web_app:app
