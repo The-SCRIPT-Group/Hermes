3aee4216fae624c6b8d2b4cd41b8652547fb3421
Akhil Narang <me@akhilnarang.dev>
2020-05-16 23:48:18 +0530

    Hermes: add a script and github workflow for auto deploy
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function run_cmd() {
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null deploy@$SERVER_IP "$*"
+}
+
+eval $(ssh-agent)
+ssh-add - <<< $SSH_KEY
+run_cmd "git -C Hermes fetch origin master"
+run_cmd "git -C Hermes reset --hard origin/master"
+run_cmd "sudo systemctl restart hermes"
+eval $(ssh-agent -k)
