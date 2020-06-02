267e34dde964ce6b1f3e259183c725b24606a328
Akhil Narang <me@akhilnarang.dev>
2020-06-03 00:37:49 +0530

    Hermes: update deps on deploy
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- shell_scripts/deploy.sh
+++ shell_scripts/deploy.sh
@@ -8,5 +8,6 @@ eval $(ssh-agent)
 ssh-add - <<< $SSH_KEY
 run_cmd "git -C Hermes fetch origin master"
 run_cmd "git -C Hermes reset --hard origin/master"
+run_cmd "/home/deploy/Hermes/venv/bin/pip install -U -r /home/deploy/Hermes/requirements.txt"
 run_cmd "sudo systemctl restart hermes"
 eval $(ssh-agent -k)
